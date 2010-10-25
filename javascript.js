f17a53da21fb8fce9cef66b5b435af52528edc61
Beaubeek <Ribeaucourt_steeve@hotmail.fr>
2010-10-26 01:42:57 +0200

    fonction date ajouté a javascript.js
    Signed-off-by: Beaubeek <Ribeaucourt_steeve@hotmail.fr>

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function date(){
+	var maintenant = new Date();
+	var year=maintenant.getFullYear();
+	var nbjour=maintenant.getDate();
+	var heure=maintenant.getHours();
+	var minute=maintenant.getMinutes();
+	var seconde=maintenant.getSeconds();
+	var lmois=new Array("Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre");
+	var ljour=new Array("Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi");
+	var mois=lmois[maintenant.getMonth()];
+	var jour=ljour[maintenant.getDay()];
+	if(minute<10)minute="0"+minute;
+	if(seconde<10)seconde="0"+seconde;
+	document.getElementById("menu").innerHTML ="<b>"+jour+" "+nbjour+" "+mois+" "+year+" "+heure+":"+minute+":"+seconde+"</b>";
+	setTimeout("date()",1000);
+}
