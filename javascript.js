2019bf3bb1774eb15befb18aebcea8ffff7f3418
Romain Hennuyer <romainhk@gmail.com>
2010-10-26 11:36:04 +0200

    remplacement des \t ; ajout/modif de l'horloge texte

--- javascript.js
+++ javascript.js
@@ -1,16 +1,19 @@
-function date(){
-	var maintenant = new Date();
-	var year=maintenant.getFullYear();
-	var nbjour=maintenant.getDate();
-	var heure=maintenant.getHours();
-	var minute=maintenant.getMinutes();
-	var seconde=maintenant.getSeconds();
-	var lmois=new Array("Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre");
-	var ljour=new Array("Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi");
-	var mois=lmois[maintenant.getMonth()];
-	var jour=ljour[maintenant.getDay()];
-	if(minute<10)minute="0"+minute;
-	if(seconde<10)seconde="0"+seconde;
-	document.getElementById("menu").innerHTML ="<b>"+jour+" "+nbjour+" "+mois+" "+year+" "+heure+":"+minute+":"+seconde+"</b>";
-	setTimeout("date()",1000);
+date = new Object();
+date.lmois=new Array("janvier","février","mars","avril","mai","juin","juillet","août","septembre","octobre","novembre","décembre");
+date.ljour=new Array("Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi");
+
+function horloge() {
+    var maintenant = new Date();
+    var year=maintenant.getFullYear();
+    var nbjour=maintenant.getDate();
+    var heure=maintenant.getHours();
+    var minute=maintenant.getMinutes();
+    var seconde=maintenant.getSeconds();
+
+    var mois=date.lmois[maintenant.getMonth()];
+    var jour=date.ljour[maintenant.getDay()];
+    if (minute<10) minute="0"+minute;
+    if (seconde<10) seconde="0"+seconde;
+    document.getElementById("horloge").innerHTML = jour+" "+nbjour+" "+mois+" "+year+"<br />"+heure+":"+minute+":"+seconde;
+    setTimeout("horloge()",1000);
 }
