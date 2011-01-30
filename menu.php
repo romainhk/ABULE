63d25ac086991dcfde1ab99e179b4a6e76ed5b98
Romain Hennuyer <romainhk@gmail.com>
2011-01-30 18:15:27 +0100

    min : pb avec le générateur de titre de la page

--- menu.php
+++ menu.php
@@ -8,14 +8,14 @@ require('uploads/menu.html');
 if (isset($_SESSION['login'])) {
     echo '<h2>Admin</h2>'."\n";
     echo '<ul>';
-    echo '<li><a href="?page=\&action=ajouter">Ajouter</a></li>';
-    echo '<li><a href="?page=\&action=supprimer">Supprimer</a></li>';
-    echo '<li><a href="?page=\&action=renommer">Renommer</a></li>';
-    echo '<li><a href="?page=\&action=lister">Liste des pages</a></li>';
-    echo '<li><a href="?page=\&action=uploader">Upload</a></li>';
-    echo '<li><a href="?page=\&action=listerup">Liste des fichiers</a></li>';
+    echo '<li><a href="?page=&action=ajouter">Ajouter</a></li>';
+    echo '<li><a href="?page=&action=supprimer">Supprimer</a></li>';
+    echo '<li><a href="?page=&action=renommer">Renommer</a></li>';
+    echo '<li><a href="?page=&action=lister">Liste des pages</a></li>';
+    echo '<li><a href="?page=&action=uploader">Upload</a></li>';
+    echo '<li><a href="?page=&action=listerup">Liste des fichiers</a></li>';
     if (!strcmp($_SESSION['login'], 'admin')) {
-        echo '<li><a href="?page=\&action=admin">Admin</a></li>';
+        echo '<li><a href="?page=&action=admin">Admin</a></li>';
     }
     echo "</ul>\n";
 }
