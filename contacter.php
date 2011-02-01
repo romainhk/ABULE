94ac52ad8537b2d742b85c97d9adac9a3088b788
Romain Hennuyer <romainhk@gmail.com>
2011-02-01 14:59:05 +0100

    mise en forme du formulaire de contact

--- contacter.php
+++ contacter.php
@@ -36,19 +36,24 @@ if (isset($_POST['from'])) {
 } else {
 echo"<h1>Contactez-nous</h1>&nbsp
 <form name='contact' method='post' action=''> 
-	<div id=form style='width:90%; margin:auto;'>
-		A qui est destin&eacute votre mail :
-		<p><input type='radio' name='to' value='admin' id='webmaster' /> <label for='to'>Le webmaster</label><br />
-        <input type='radio' name='to' value='labule' id='asso' checked=\"checked\" /> <label for='asso'>Les membres de L&#180;A.B.U.L.E.</label><br /></p>
-		<label for='from'>Votre e-mail</label> : 
-		<input type='text' name='from' id='from' size='30'></input><br/><br/>
-		<label for='subject'>Objet</label> : 
-		<input type='text' name='subject' id='subject' size='60'></input><br/>&nbsp
-		<div id='text' style='text-align:right'>
-			<textarea name='mess' id='mess' rows='13' style='width:100%; border:3px double brown;' onfocus='efface()'>Votre message ici.</textarea><br/><br/>
-			<input type='button' value='Envoyer' onClick='verif_mail()' style=\"font-weight:600;\"></input>
-		</div>
-
+    <div class=\"contact\" id=form>
+    <ul>
+		<li>&#192; qui est destin&eacute votre mail :<ul>
+            <li><input type='radio' name='to' value='admin' id='webmaster' />
+                <label for='to'>Le webmaster</label></li>
+            <li><input type='radio' name='to' value='labule' id='asso' checked=\"checked\" />
+                <label for='asso'>Les membres de L&#180;A.B.U.L.E.</label></li>
+            </ul>
+		<li><label for='from'>Votre e-mail</label> : 
+		<input type='text' name='from' id='from' size='30'></input></li>
+		<li><label for='subject'>Objet</label> : 
+		<input type='text' name='subject' id='subject' size='60'></input></li>
+		<li><label for='mess'>Message</label> : 
+		    <div id='text' style='text-align:right'>
+<textarea name='mess' id='mess' rows='13' style='width:100%; border:3px double brown;' onfocus='efface()'>Votre message ici.</textarea><br/>
+<input type='button' value='Envoyer' onClick='verif_mail()' style=\"font-weight:600;\"></input>
+    		</div></li>
+    </ul></div>
 </form>";
 }
 ?>
